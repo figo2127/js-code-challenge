a4329049977df39607c6dcc0fd1c620831b0f73a
figo2127 <61351392+figo2127@users.noreply.github.com>
2021-04-12 17:51:27 +0800

    push solutions

--- src/datasource_connector_tool/solution.js
+++ src/datasource_connector_tool/solution.js
@@ -1,3 +1,72 @@
 // Problem 2
 // Task: Implement a datasource connector to abstract away data retrieval and manipulation from the `ViewControllers`.  
 // Your solution shall use only [Vanilla JavaScript](http://vanilla-js.com).  
+
+// global.fetch = require("node-fetch");
+// var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+class Price {
+    constructor(buy, sell, id, pair, timestamp) {
+        this.buy = buy
+        this.sell = sell
+        this.id = id
+        this.pair = pair
+        this.timestamp = timestamp
+    }
+    mid() {
+        let x = (this.buy + this.sell) / 2
+        return x
+    }
+    quote() {
+        var amt = "";
+        //retrieve the currency:
+        for (var i = 3; i < this.pair.length; i++) {
+            // console.log(this.pair)
+            amt += this.pair[i]
+        }
+        return amt;
+    }
+} 
+// fetch() is equivalent of request.open() in XHR
+// fetch() returns a Promise
+// Datasource Controller
+class Datasource {
+    getPrices() {
+        return fetch('https://static.ngnrs.io/test/prices')
+            .then(res => {
+                if (res.ok) {
+                    console.log('SUCCESS')
+                }
+                else {
+                    console.log('Not successful')
+                }
+                return res.json()
+            }).then(data => {
+                // console.log(data['data']);
+                var coin_currency_arr = [];
+                let data_arr = data['data']['prices'];
+                // console.log(data_arr)
+                
+                for (var i = 0; i < data_arr.length; i++) {
+                    let price_ds = new Price(data_arr[i].buy, data_arr[i].sell, data_arr[i].id, data_arr[i].pair, data_arr[i].timestamp);
+                    coin_currency_arr.push(price_ds);
+                }
+                return coin_currency_arr;
+        }, err => {
+            return err;
+        })
+  }
+}
+
+const fetch = require('node-fetch');
+
+// Abstraction 
+let ds = new Datasource();
+ds.getPrices()
+    .then(prices => {
+        prices.forEach(price => {
+            console.log(`Mid price for ${ price.pair } is ${ price.mid() } ${ price.quote() }.`);
+        });
+    }).catch(error => {
+        console.err(error);
+    });
--- src/three_ways_to_sum_to_n/solution_a.js
+++ src/three_ways_to_sum_to_n/solution_a.js
@@ -5,4 +5,19 @@
 
 var sum_to_n = function(n) {
     // your code here
+
+    if (n < 0) {
+        console.log("negative number");
+        return -1;
+    }
+
+    var sum = 0;
+    for (var i = 1; i <= n; i++) {
+        sum += i;
+    }
+    
+    console.log(sum);
+    return sum;
 };
+
+sum_to_n(10); //input here
